2728844c6438f6ec3b91c4c868bb7a1dbd4b7f05
cauanlsreis <cauanlucas.reis@gmail.com>
2024-07-30 22:09:22 -0300

    Extraindo coordenadas

--- personal.py
+++ personal.py
@@ -23,6 +23,13 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+        #Extraindo os pontos de referência
+        try:
+            landmarks = results.pose_landmarks.landmark
+            print(landmarks)
+        except:
+            pass
+
         #Renderizando as detecções
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                   mp_drawing.DrawingSpec(color=(57, 244, 1), thickness=4, circle_radius=2),
